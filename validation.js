ef25168d7961203f6ae0c65289dd834bc7fb3647
Draaakos <orlando.andaur.c@gmail.com>
2018-10-30 23:47:50 -0300

    added regular expression to date

--- validation.js
+++ validation.js
@@ -1,16 +1,26 @@
+function redBorder(id) {
+    const body = document.querySelector("body")
+
+    body.classList.add(`${id}-incorrect`);
+    return false;
+}
+
 function validateValue(id, regex) {
     const value = document.querySelector(`#${id}`).value;
-    return regex.test(value);
+
+    return (!regex.test(value) || !value.length)
+        ? redBorder(id) : true
 }
 
 function validation() {
+    let validate = true;
     const REGEX_NUMBER = /\d/;
-    const REGEX_NAME = /\cX/;
+    const REGEX_DATE = /\d{2}\/\d{2}\/\d{4}/;
 
-    const date = validateValue('date',REGEX_NUMBER);
-    const monthSalary = validateValue('month-salary', REGEX_NUMBER);
-    const approbeSalary = validateValue('approbe-salary',REGEX_NUMBER);
-    const dateContratation = validateValue('date-contratation',REGEX_NUMBER);
+    validate = validateValue('date', REGEX_DATE);
+    // const monthSalary = validateValue('month-salary', REGEX_NUMBER);
+    validate =  validateValue('approbe-salary', REGEX_NUMBER);
+    // const dateContratation = validateValue('date-contratation',REGEX_NUMBER);
 
-    return false;
+    return validate;
 }
