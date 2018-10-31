ad9ae574da66500c9aa1bdeab40501ea6b7392b5
linaowo <linaowo@gmail.com>
2018-10-30 23:13:30 -0300

    cambios

--- validation.js
+++ validation.js
@@ -5,8 +5,12 @@ function validateValue(id, regex) {
 
 function validation() {
     const REGEX_NUMBER = /\d/;
+    const REGEX_NAME = /\cX/;
 
+    const date = validateValue('date',REGEX_NUMBER);
     const monthSalary = validateValue('month-salary', REGEX_NUMBER);
+    const approbeSalary = validateValue('approbe-salary',REGEX_NUMBER);
+    const dateContratation = validateValue('date-contratation',REGEX_NUMBER);
 
     return false;
 }
